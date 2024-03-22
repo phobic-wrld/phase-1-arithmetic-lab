481cfb1e40496601f824d6b01fc7b6371d3f9b74
joy <blackbearmode1@gmail.com>
2024-03-22 21:55:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31;
+let num2 = 2;
+let multiply =  num1*num2;
+let random = Math.floor(Math.random()) + 1;
+let num3 = 16;
+let num4 = 6;
+let mod= num3 % num4;
+let max = Math.max(1, 5, 10, 15, 20);
 // Write your code here
